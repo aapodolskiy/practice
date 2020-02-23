f5d0d9edc5b66d23366240f0722abce6aef5e668
Alexander <a.a.podolskiy@gmail.com>
2020-02-24 00:21:28 +0300

    create, print and destruct

--- BinarySearchTrees.cpp
+++ BinarySearchTrees.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ struct Elem {
 };
 
 typedef Elem * PElem;
+
+// для альтернативного способа печати в виде ярусов
+//bool is_power_of_2(int n) { return n > 0 && (n & (n - 1)) == 0; }
 
 PElem Create(int x) {
     PElem b = new Elem;
@@ -25,63 +29,197 @@ PElem Create() {
 class SearchTree {
 private:
     PElem root;
+    
+    static void addElement(PElem &p, int a) {
+        if (p == NULL) {
+            p = new Elem;
+            p->data = a;
+            p->left = NULL;
+            p->right = NULL;
+            return;
+        }
+        
+        if (a < p->data) {
+            SearchTree::addElement(p->left, a);
+            return;
+        }
+        
+        if (p->data < a) {
+            SearchTree::addElement(p->right, a);
+            return;
+        }
+    }
+    
+    static int getHeight(const PElem &p) {
+        if (p == NULL) {
+            return 0;
+        }
+        
+        return 1 + max(SearchTree::getHeight(p->left), SearchTree::getHeight(p->right));
+    }
+    
+    static void fillTiers(const PElem &p, int current_position, int * array_of_tiers) {
+        if (p == NULL) {
+            return;
+        }
+        array_of_tiers[current_position] = p->data;
+        SearchTree::fillTiers(p->left, 2*current_position, array_of_tiers);
+        SearchTree::fillTiers(p->right, 2*current_position + 1, array_of_tiers);
+    }
+    
+    static void fillTiersMoreBeautiful(const PElem &p, int shift, int height, int * array_of_tiers) {
+        if (p == NULL) {
+            return;
+        }
+        int p_position = shift + pow(2, height-1);
+        array_of_tiers[p_position] = p->data;
+        SearchTree::fillTiersMoreBeautiful(p->left, shift, height-1, array_of_tiers);
+        SearchTree::fillTiersMoreBeautiful(p->right, p_position, height-1, array_of_tiers);
+    }
+    
+    static void increasingPrint(const PElem &p) {
+        if (p == NULL) {
+            return;
+        }
+        increasingPrint(p->left);
+        cout << p->data << " ";
+        increasingPrint(p->right);
+    }
+    
+    static void decreasingPrint(const PElem &p) {
+        if (p == NULL) {
+            return;
+        }
+        decreasingPrint(p->right);
+        cout << p->data << " ";
+        decreasingPrint(p->left);
+    }
+    
+    static void freeMemory(PElem &p) {
+        if (p == NULL) {
+            return;
+        }
+        freeMemory(p->left);
+        freeMemory(p->right);
+        delete p;
+    }
 public:
     SearchTree() {
         root = NULL;
-    }
-    SearchTree(PElem r) {
-        root = r;
     }
     
     bool isEmpty() {
         return root == NULL;
     }
     
-    PElem addElement(int a) {
-        if (this->isEmpty()) {
-            root = new Elem;
-            root->data = a;
-            return root;
-        } else {
-            if (a == root->data) {
-                return NULL;
+    SearchTree(int elements[], int length) {
+        root = NULL;
+        for (int i = 0; i < length; i++) {
+            SearchTree::addElement(root, elements[i]);
+        }
+    }
+    
+    void printTiers() {
+        int h = SearchTree::getHeight(root);
+        if (h == 0) {
+            cout << "tree is empty" << endl;
+        }
+        
+        int total_length = pow(2, h);
+        int tiers[total_length];
+        int dummy_value = -1;
+        for (int i = 0; i < total_length; i++) {
+            tiers[i] = dummy_value;
+        }
+        
+        SearchTree::fillTiers(root, 1, tiers);
+        
+        const char free_space_character = '-';
+
+        for (int i = 0; i < h; i++) {
+            int t = pow(2, i);
+            for (int j = 0; j < t; j++) {
+                if (tiers[t + j] == dummy_value) {
+                    cout << free_space_character;
+                } else {
+                    cout << tiers[t + j];
+                }
+                cout << string(pow(2, h-i+1)-1, free_space_character);
             }
-            
-            if (a < root->data) {
-                root->left = SearchTree(root->left).addElement(a);
-                return root;
-            } else {
-                root->right = SearchTree(root->right).addElement(a);
-                return root;
+            if (i != h-1) {
+                cout << endl;
             }
         }
     }
     
-    SearchTree(int a[], int length) {
-        for(int i = 0; i < length; i++) {
-            cout << endl << i << ": ";
-            addElement(a[i]);
+    void printTiersMoreBeautiful() {
+        int h = SearchTree::getHeight(root);
+        if (h == 0) {
+            cout << "tree is empty" << endl;
+        }
+        
+        int total_length = pow(2, h);
+        int tiers[total_length];
+        int dummy_value = -1;
+        for (int i = 0; i < total_length; i++) {
+            tiers[i] = dummy_value;
+        }
+        
+        SearchTree::fillTiersMoreBeautiful(root, 0, h, tiers);
+        
+        const char free_space_character = ' ';
+
+        for (int i = h; i > 0; i--) {
+            int step = pow(2, i);
+            int position = step / 2;
+            while (position < total_length) {
+                cout << string(min(position, step)-1, free_space_character);
+                if (tiers[position] != dummy_value) {
+                    cout << tiers[position];
+                } else {
+                    cout << free_space_character;
+                }
+                position += step;
+            }
+        
+            if (i != 1) {
+                cout << endl;
+            }
         }
     }
     
-    void print() {
-        if (this->isEmpty()) {
-            return;
-        }
-        cout << root -> data << " ";
-        SearchTree(root->left).print();
-        SearchTree(root->right).print();
+    void increasingPrint() {
+        SearchTree::increasingPrint(root);
+    }
+    
+    void decreasingPrint() {
+        SearchTree::decreasingPrint(root);
     }
     
     ~SearchTree() {
+        SearchTree::freeMemory(root);
     }
 };
 
 int main() {
-    cout << "aa";
-    int t[6] = {1, 2, 3, 4, 5};
-    SearchTree a(t, 4);
-    cout << endl << "print: " << endl;
-    a.print();
+    int t[10] = {3, 5, 1, 2, 4, 7, 8, 0, 11, 6};
+    SearchTree a(t, 10);
+    
+    cout << endl << "print tiers: " << endl;
+    a.printTiers();
+    cout << endl;
+    
+    cout << endl << "print tiers more beautiful: " << endl;
+    a.printTiersMoreBeautiful();
+    cout << endl;
+    
+    cout << endl << "print in increasing order: " << endl;
+    a.increasingPrint();
+    cout << endl;
+    
+    cout << endl << "print in decreasing order: " << endl;
+    a.decreasingPrint();
+    cout << endl;
+    
     return 0;
 }
